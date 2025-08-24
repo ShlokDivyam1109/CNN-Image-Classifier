e74578178875bde0b59f241913f9aa83c368a2ae
ShlokDivyam1109 <shlokd@iitbhilai.ac.in>
2025-08-24 16:16:03 +0530

    Save changes to CNN_Model_Trainer.py

--- CNN_Model_Trainer.py
+++ CNN_Model_Trainer.py
@@ -32,12 +32,9 @@ def build_model(num_classes, input_shape):
 		layers.Conv2D(32, (3,3), activation='relu'),
 		layers.MaxPooling2D(2,2),
 		layers.Conv2D(64, (3,3), activation='relu'),
-		layers.MaxPooling2D(2,2),
-		layers.Flatten(),
-		layers.Conv2D(64, (3,3), activation='relu'),
-		layers.MaxPooling2D(2,2),
-		layers.Flatten(),
-		layers.Dense(128, activation='relu'),
+        layers.MaxPooling2D(2,2),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
 		layers.Dense(num_classes, activation='softmax')
 	])
 	model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
